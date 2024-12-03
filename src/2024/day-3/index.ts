2de13a10b65ff2dec35b4dae0557bfffdd07b567
Katie Raby <katie.raby@icloud.com>
2024-12-03 23:30:42 +0000

    day 3 - part two solution complete

--- src/2024/day-3/index.ts
+++ src/2024/day-3/index.ts
@@ -3,20 +3,37 @@ import path from 'path';
 
 const input = fs.readFileSync(path.resolve(__dirname, `./input.txt`), 'utf8');
 
-const multiply = (a: number, b: number) => a * b;
-
 export const getResultsOfMultiplications = (str: string): number => {
   let totalOfMultiplications = 0;
 
+  const doOrDontPattern = /(do\(\))|(don\'t\(\))/g;
+  const arraySplitByDosAndDonts = str.split(doOrDontPattern);
+  let allMuls: string[] = [];
   const regexpForAllMuls = /(mul\(\d+,\d+\))/g;
-  const allMuls = str.match(regexpForAllMuls);
-  if (!allMuls) return 0;
+  let doProceed = true;
+
+  for (let i = 0; i < arraySplitByDosAndDonts.length; i++) {
+    if (!arraySplitByDosAndDonts[i]) continue;
+    if (doProceed && !arraySplitByDosAndDonts[i].includes("don't()")) {
+      const match = arraySplitByDosAndDonts[i].match(regexpForAllMuls);
+      if (!match) continue;
+      allMuls = [...allMuls, ...match];
+    }
+
+    if (arraySplitByDosAndDonts[i].includes("don't()")) {
+      doProceed = false;
+    }
+
+    if (arraySplitByDosAndDonts[i].includes('do()')) {
+      doProceed = true;
+    }
+  }
 
   const mulDigitsPattern = /\((\d+),(\d+)\)/;
   for (let i = 0; i < allMuls.length; i++) {
     const match = allMuls[i].match(mulDigitsPattern);
     if (!match) break;
-    const result = multiply(+match[1], +match[2]);
+    const result = +match[1] * +match[2];
     totalOfMultiplications += result;
   }
 
